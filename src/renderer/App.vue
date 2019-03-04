85eb45a650d965af1017f02a061ff91ac3d75cca
ergou_xiang <wx_a54ed7e43dcd45d68c792fec474ebf05@git.code.tencent.com>
2019-03-04 14:02:36 +0800

    初始化

---
+++ src/renderer/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'electron-quick-start'
+  }
+</script>
+
+<style>
+@import './style/index.scss';
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background: #F6F6F6;
+  overflow: auto;
+}
+</style>
---
+++ src/renderer/views/layout/Layout.vue
@@ -0,0 +1,43 @@
+/*
+ * @Author: liuxia
+ * @Date: 2019-03-04 11:14:55
+ * @Last Modified by: liuxia
+ * @Last Modified time: 2019-03-04 11:51:43
+ */
+<template>
+  <div class="app-wrapper">
+    <background></background>
+    <header>
+      header
+    </header>
+    <aside>
+      aside
+    </aside>
+    <article>
+      article
+    </article>
+    <footer>
+      footer
+    </footer>
+  </div>
+</template>
+
+<script>
+import background from '@/components/background.vue'
+export default {
+  components: {
+    background
+  }
+}
+</script>
+
+<style lang="scss">
+  .app-wrapper{
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 9999;
+  }
+</style>
